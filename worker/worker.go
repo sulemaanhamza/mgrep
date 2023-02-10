4b7a16c64d63f695faa9a4a600a2709ce68a58d0
Suleman <sulemanhamza97@gmail.com>
2023-02-10 10:40:26 +0500

    Implemented findInFiles method to scan the file and return results

--- worker/worker.go
+++ worker/worker.go
@@ -1,5 +1,12 @@
 package worker
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Result struct {
 	Line    string
 	LineNum int
@@ -12,4 +19,32 @@ type Results struct {
 
 func NewResult(line string, lineNum int, path string) Result {
 	return Result{line, lineNum, path}
+}
+
+func FindInFile(path string, find string) *Results {
+	file, err := os.Open(path)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	results := Results{make([]Result, 0)}
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), find) {
+			r := Result{scanner.Text(), lineNum, path}
+			results.Inner = append(results.Inner, r)
+		}
+		lineNum++
+	}
+
+	if len(results.Inner) == 0 {
+		return nil
+	} else {
+		return &results
+	}
 }
