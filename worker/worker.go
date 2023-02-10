ab1d236a0d802e500cdade87e2135ac35b291e05
Suleman <sulemanhamza97@gmail.com>
2023-02-10 10:31:27 +0500

    Basic implementation of result structure

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,15 @@
 package worker
+
+type Result struct {
+	Line    string
+	LineNum int
+	Path    string
+}
+
+type Results struct {
+	Inner []Result
+}
+
+func NewResult(line string, lineNum int, path string) Result {
+	return Result{line, lineNum, path}
+}
